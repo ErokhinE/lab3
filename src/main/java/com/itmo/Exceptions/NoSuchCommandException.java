ffa68e9ca7a8bd33424d31a30a3021c8629a4938
Mikhaylovigor <62118662+Mikhaylovigor@users.noreply.github.com>
2022-02-26 11:20:34 +0300

    Delete src/main/java/com/itmo/Exceptions directory

--- src/main/java/com/itmo/Exceptions/NoSuchCommandException.java
+++
@@ -1,7 +0,0 @@
-package com.itmo.Exceptions;
-
-public class NoSuchCommandException extends RuntimeException {
-    public NoSuchCommandException(String badCommand){
-        System.out.println("Такой команды \""+badCommand+"\" нет.");
-    }
-}
